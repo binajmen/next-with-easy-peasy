cee6408c76806b85e0156ddb245a4936795acec7
Benjamin Decoster <bdcstr@gmail.com>
2021-02-18 08:04:14 +0100

    use getActions() in getXXXProps

--- pages/other.js
+++ pages/other.js
@@ -10,8 +10,7 @@ const Other = (props) => {
 export const getServerSideProps = wrapper.getServerSideProps(
     ({ store }) => {
         console.log('ssr init count to 8')
-        // 👇 is there a way to dispatch EP-style ? (eg: actions.initCount(8))
-        store.dispatch({ type: '@action.first.initCount', payload: 8 })
+        store.getActions().first.initCount(8)
     }
 )
 
