48c8768ba103bf409f1e73bebfb2d4ccbfe6f235
Benjamin Decoster <bdcstr@gmail.com>
2021-02-18 10:23:19 +0100

    fix hydration

--- components/Basket.js
+++ components/Basket.js
@@ -1,21 +1,24 @@
 import * as React from 'react'
-import { useStoreState } from 'easy-peasy'
+import { useStoreRehydrated, useStoreState } from 'easy-peasy'
 
 function Basket() {
     const basket = useStoreState(state => state.shop.basket)
+    const isRehydrated = useStoreRehydrated()
 
-    React.useEffect(() => console.log(basket), [basket])
-
-    return (
-        <div style={{ margin: '2rem' }}>
-            <h1>Basket</h1>
-            <ul>
-                {Object.entries(basket).map(([item, quantity], index) =>
-                    <li key={index}>{item} x {quantity}</li>
-                )}
-            </ul>
-        </div>
-    )
+    if (isRehydrated) {
+        return (
+            <div style={{ margin: '2rem' }}>
+                <h1>Basket</h1>
+                <ul>
+                    {Object.entries(basket).map(([item, quantity], index) =>
+                        <li key={index}>{item} x {quantity}</li>
+                    )}
+                </ul>
+            </div>
+        )
+    } else {
+        return null
+    }
 }
 
 export default Basket
