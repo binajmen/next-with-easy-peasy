8fcc16a949660357280a2c6db9e3f52bac863fb1
Benjamin Decoster <bdcstr@gmail.com>
2021-02-18 14:05:22 +0100

    useStoreRehydrated

--- components/Basket.js
+++ components/Basket.js
@@ -1,19 +1,15 @@
-import { useStoreState } from 'easy-peasy'
+import * as React from 'react'
+import { useStoreRehydrated, useStoreState } from 'easy-peasy'
 
 function Basket() {
-    const shop = useStoreState(state => state.shop)
     const basket = useStoreState(state => state.shop.basket)
-
-    // When basket is not empty, although the store is populated on first render (non-empty in console),
-    // the Object().map()ing output is empty. Only when you click on the links, the basket is showing
-    console.log('shop:', shop)
-    console.log('shop.basket:', basket)
+    const isRehydrated = useStoreRehydrated()
 
     return (
         <div style={{ margin: '2rem' }}>
             <h1>Basket</h1>
             <ul>
-                {Object.entries(shop.basket).map(([item, quantity], index) =>
+                {isRehydrated && Object.entries(basket).map(([item, quantity], index) =>
                     <li key={index}>{item} x {quantity}</li>
                 )}
             </ul>
